d78177adb36dc8cfbe39438374288c2539a395b4
tvinente <vinente.talyta@gmail.com>
2023-01-30 16:37:12 -0300

    Exercício aula03 - Métodos (Return)

---
+++ metodo-aula-03/src/Quadrilatero.java
@@ -0,0 +1,21 @@
+/**
+ * Classe de exemplo para o exercício da Aula 3 de Métodos.
+ */
+
+public class Quadrilatero {
+
+    public static double area(double lado) {
+
+        return lado * lado;
+    }
+
+    public static double area(double lado1, double lado2) {
+
+        return lado1 * lado2;
+    }
+
+    public static double area(double baseMaior, double baseMenor, double altura) {
+
+        return (baseMaior + baseMenor) * altura / 2;
+    }
+}
